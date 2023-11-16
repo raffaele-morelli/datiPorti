426cd53bf1caeb4b8fd40720a6e1c7c025e10e6b
raffaele-morelli <raffaele.morelli@isprambiente.it>
2023-11-16 11:18:14 +0100

    documentazione parziale

--- R/df_nhr.R
+++ R/df_nhr.R
@@ -4,16 +4,22 @@
 #'
 #' @format A tibble with 243 rows and 10 variables:
 #' \describe{
-#'   \item{Year}{dbl Year price was recorded}
-#'   \item{Month}{dbl Month price was recorded. Ranges from 1-12 for January - December}
-#'   \item{`0-15`}{dbl denoting monthly price for the 0-15/lb. size class}
-#'   \item{`15-20`}{dbl denoting monthly price for the 15-20/lb. size class}
-#'   \item{`20-25`}{dbl denoting monthly price for the 20-25/lb. size class}
-#'   \item{`25-30`}{dbl denoting monthly price for the 25-30/lb. size class}
-#'   \item{`30-40`}{dbl denoting monthly price for the 30-40/lb. size class}
-#'   \item{`40-50`}{dbl denoting monthly price for the 40-50/lb. size class}
-#'   \item{`50-67`}{dbl denoting monthly price for the 50-67/lb. size class}
-#'   \item{Pieces}{dbl denoting monthly price of pieces of shrimp totalling a pound}
+#'   \item{pltnt}{chr l'inquinante}
+#'   \item{irec}{dbl }
+#'   \item{gt}{dbl }
+#'   \item{terminal}{dbl }
+#'   \item{Ship_categ}{dbl }
+#'   \item{ntug}{dbl }
+#'   \item{Emiss_HOT_MDGO}{dbl }
+#'   \item{Emiss_MAN_MDGO}{dbl }
+#'   \item{Emiss_tug_MDGO}{dbl }
+#'   \item{Emiss_Cter_MDGO}{dbl }
+#'   \item{Emiss_Cext_MDGO}{dbl }
+#'   \item{Emiss_HOT_BFO}{dbl }
+#'   \item{Emiss_MAN_BFO}{dbl }
+#'   \item{Emiss_tug_BFO}{dbl }
+#'   \item{Emiss_Cter_BFO}{dbl }
+#'   \item{Emiss_Cext_BFO}{dbl }
 #' }
 #' @source \url{https://www.pnas.org/content/114/7/1512/tab-figures-data}
 "df_nhr"
